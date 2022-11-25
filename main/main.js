396ad4ebdc541f8c88e4ddf0e6cabd3a0efa4d34
Muhammadbey <mahkamovmuhammadbey@gmail.com>
2022-11-25 22:27:12 +0500

    first commit'none'

---
+++ main/main.js
@@ -0,0 +1,18 @@
+var name;
+name = "akmal"//string
+var age;
+age = 12; //number
+var  lastName // underfined
+var isPupli = false; //boolean
+var car = {
+  model: 'tesla',
+  year: "2022",
+  color:"red",
+}; //object
+console.log(typeof isPupli);
+var a = 5;
+
+var b = "2"
+var c = parseInt (a) + parseInt (b) 
+console.log(c)
+cn
